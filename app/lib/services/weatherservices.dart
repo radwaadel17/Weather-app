4b82fb4b05a729ebf119fc66faf560710f4dc3d0
radwaadel17 <radwaadel959@gmail.com>
2024-08-09 20:21:45 +0300

    weatherService 1

--- app/lib/services/weatherservices.dart
+++ app/lib/services/weatherservices.dart
@@ -1,11 +1,12 @@
+import 'package:app/models/weathermodel.dart';
 import 'package:dio/dio.dart';
 
 class weatherService {
   final Dio dio ;
   const weatherService(this.dio);
-  
-  void getHttp() async {
-  final response = await dio.get('http://api.weatherapi.com/v1/forecast.json?key=d9c25a6552f64bb4bcc183308240708&q=Cairo&days=1&aqi=no&alerts=no');
+  Future<weathermodel> GetRequest(String Value)async {
+  final response = await dio.get('http://api.weatherapi.com/v1/forecast.json?key=d9c25a6552f64bb4bcc183308240708&q=$Value&days=1&aqi=no&alerts=no');
+  Map<dynamic , dynamic > json = response.data ;
+  return weathermodel.fromjson(json);
 }
-
 }
