45cd32781f50b60eb0e3029ef96555b3f48839c1
radwaadel17 <radwaadel959@gmail.com>
2024-08-09 20:56:12 +0300

    Add base url to improve code

--- app/lib/services/weatherservices.dart
+++ app/lib/services/weatherservices.dart
@@ -3,9 +3,10 @@ import 'package:dio/dio.dart';
 
 class weatherService {
   final Dio dio ;
+  final String BaseUrl = 'http://api.weatherapi.com/v1';
   const weatherService(this.dio);
   Future<weathermodel> GetRequest({required String cityName})async {
-  Response response = await dio.get('http://api.weatherapi.com/v1/forecast.json?key=d9c25a6552f64bb4bcc183308240708&q=$cityName&days=1&aqi=no&alerts=no');
+  Response response = await dio.get('$BaseUrl/forecast.json?key=d9c25a6552f64bb4bcc183308240708&q=$cityName&days=1&aqi=no&alerts=no');
   weathermodel w1 = weathermodel.fromjson(response.data);
   return w1;
 }
