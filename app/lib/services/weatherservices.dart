4a5ff04095be2f3c48de377819cdce6fa6de0ec5
radwaadel17 <radwaadel959@gmail.com>
2024-08-09 20:58:56 +0300

    Finish weatherService method

--- app/lib/services/weatherservices.dart
+++ app/lib/services/weatherservices.dart
@@ -4,9 +4,10 @@ import 'package:dio/dio.dart';
 class weatherService {
   final Dio dio ;
   final String BaseUrl = 'http://api.weatherapi.com/v1';
+  final String ApiKey ='d9c25a6552f64bb4bcc183308240708';
   const weatherService(this.dio);
   Future<weathermodel> GetRequest({required String cityName})async {
-  Response response = await dio.get('$BaseUrl/forecast.json?key=d9c25a6552f64bb4bcc183308240708&q=$cityName&days=1&aqi=no&alerts=no');
+  Response response = await dio.get('$BaseUrl/forecast.json?key=$ApiKey&q=$cityName&days=1&aqi=no&alerts=no');
   weathermodel w1 = weathermodel.fromjson(response.data);
   return w1;
 }
