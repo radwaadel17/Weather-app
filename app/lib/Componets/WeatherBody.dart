66347e608e0ebdaa2304ede80d54c0c8689b3112
radwaadel17 <radwaadel959@gmail.com>
2024-08-18 19:05:29 +0300

    Finish APP

--- app/lib/Componets/WeatherBody.dart
+++ app/lib/Componets/WeatherBody.dart
@@ -1,3 +1,4 @@
+import 'package:app/main.dart';
 import 'package:app/models/weathermodel.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -9,64 +10,72 @@ class WeatherBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
         final weathermodel model = BlocProvider.of<GetWeathercubit>(context).w!;
-    return  Center(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Text(
-            model.city,
-            style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-          ),
-          
-          Text(
-            'updated at ${model.last_update}',
-            style: TextStyle(fontSize: 23),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Image(image: AssetImage('assets/cloudy.png')),
-              Padding(
-                padding: EdgeInsets.only(left: 20),
-                child: Text(
-                  '${model.avg_temp}',
-                  style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+    return  Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(begin:Alignment.topCenter , end:Alignment.bottomCenter , colors:[
+          getWeatherColor(model.condition),
+          getWeatherColor(model.condition)[300]!,
+        ] ),
+      ),
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              model.city,
+              style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            
+            Text(
+              'updated at ${model.last_update}',
+              style: TextStyle(fontSize: 23),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Image(image: AssetImage('assets/cloudy.png')),
+                Padding(
+                  padding: EdgeInsets.only(left: 20),
+                  child: Text(
+                    '${model.avg_temp}',
+                    style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                  ),
                 ),
-              ),
-              Column(
-                children: [
-                  Text(
-                    'Max temp : ${model.mn_temp}',
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 17,
+                Column(
+                  children: [
+                    Text(
+                      'Max temp : ${model.mn_temp}',
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 17,
+                      ),
                     ),
-                  ),
-                  Text(
-                    'Max temp : ${model.mx_temp}',
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 17,
+                    Text(
+                      'Max temp : ${model.mx_temp}',
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 17,
+                      ),
                     ),
-                  ),
-                ],
-                
-              )
-            ],
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          Text( model.condition , 
-           style: const TextStyle(
-            fontSize: 30 , 
-            fontWeight: FontWeight.bold
-           ),
-          )
-        ],
+                  ],
+                  
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Text( model.condition , 
+             style: const TextStyle(
+              fontSize: 30 , 
+              fontWeight: FontWeight.bold
+             ),
+            )
+          ],
+        ),
       ),
     );
   }
